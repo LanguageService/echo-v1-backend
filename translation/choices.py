65b77c896a68bc7e606f05e21d3b296497de803b
Sunday <sunnexajayi@gmail.com>
2026-01-18 23:39:08 +0200

    fix feature type

--- translation/choices.py
+++ translation/choices.py
@@ -2,14 +2,12 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+
 class FeatureType(models.TextChoices):
     """
     Feature Types
     """
 
-    REGISTRATION = "REGISTRATION", _("REGISTRATION")
-    RESET_PASSWORD = "RESET PASSWORD", _("RESET PASSWORD")
-    LOGIN = "LOGIN", _("LOGIN")
 
     SPEECH_TRANSLATION = "SPEECH_TRANSLATION", _("SPEECH_TRANSLATION")
     IMAGE_TRANSLATION = "IMAGE_TRANSLATION", _("IMAGE_TRANSLATION")
